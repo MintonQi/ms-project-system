1614a09b67f0504593d8eb70f4b9e0bef45c5805
Qi Mingtong <qimingtong518@gmail.com>
2023-03-24 07:29:40 +0800

    logging configured

--- src/main/java/com/minton/system/SystemApplication.java
+++ src/main/java/com/minton/system/SystemApplication.java
@@ -1,14 +1,16 @@
 package com.minton.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.minton.system", "com.minton.logging"})
+@MapperScan(basePackages = {"com.minton.system", "com.minton.logging"})
 public class SystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class, args);
         System.out.println("hello!");
     }
-
 }
---
+++ src/main/java/com/minton/system/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.minton.system.controller;
+
+
+import com.minton.common.ret.RetInfo;
+import com.minton.common.ret.RetResult;
+import com.minton.logging.OperLog;
+import com.minton.system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/test")
+    @OperLog(operationDesc = "测试查询admin")
+    public RetInfo testSelectUser() throws IllegalStateException {
+        String s = userService.testSelect();
+        throw new IllegalStateException();
+//        return RetResult.retSuccess(s);
+    }
+
+}
